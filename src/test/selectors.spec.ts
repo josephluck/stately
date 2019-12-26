03f9fe1f2dbb462618af1b90b1f6b50bb3808513
Joseph Luck <joseph.luck@sky.com>
2019-12-26 15:47:22 +0000

    Add additional selector tests

--- src/test/selectors.spec.ts
+++ src/test/selectors.spec.ts
@@ -9,18 +9,18 @@ const model = {
   }
 };
 
-test("selectors / selection is memoised when state is not changed", t => {
+test("selectors / selection is memoised when unrelated state is changed", t => {
   t.plan(1);
   const store = stately(model);
   const changeA = store.createMutator((state, a: string) => (state.a = a));
-  const selectB = store.createSelector((state, a?: string) => state.b);
+  const selectB = store.createSelector(state => state.b);
   const first = selectB();
   changeA("aa");
   const second = selectB();
   t.strictEqual(
     second,
     first,
-    "selector returns strict equality compatible selector"
+    "simple selector returns strict equality compatible selector"
   );
 });
 
@@ -29,13 +29,53 @@ test("selectors / selection is not memoised when state is changed", t => {
   const store = stately(model);
   store.subscribe(console.log);
   const changeC = store.createMutator((state, c: string) => (state.b.c = c));
-  const selectB = store.createSelector((state, a?: string) => state.b);
+  const selectB = store.createSelector(state => state.b);
   const first = selectB();
   changeC("cc");
   const second = selectB();
   t.notStrictEqual(
     second,
     first,
+    "simple selector returns strict equality compatible selector"
+  );
+});
+
+test("selectors / selection with arguments is memoised when arguments are the same", t => {
+  t.plan(2);
+  const store = stately(model);
+  const selectAndAddToD = store.createSelector(
+    (state, d: number) => state.b.d + d
+  );
+  const first = selectAndAddToD(1);
+  t.equal(first, 2, "selector returns state with argument applied");
+  const second = selectAndAddToD(1);
+  t.strictEqual(
+    second,
+    first,
     "selector returns strict equality compatible selector"
+  );
+});
+
+test("selectors / selection with arguments that affect selection is memoised when arguments are the same", t => {
+  t.plan(2);
+  const store = stately(model);
+  const changeA = store.createMutator((state, a: string) => (state.a = a));
+  const selectKey = store.createSelector(
+    (state, key: keyof typeof model) => state[key]
+  );
+  const first = selectKey("b");
+  const second = selectKey("a");
+  changeA("aa");
+  const third = selectKey("b");
+  const fourth = selectKey("a");
+  t.strictEqual(
+    third,
+    first,
+    "selector returns strict equality compatible selector when state has not changed"
+  );
+  t.notStrictEqual(
+    fourth,
+    second,
+    "selector returns strict equality compatible selector when state has changed"
   );
 });
