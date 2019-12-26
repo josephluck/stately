3360a58036a7cce4099cf8837a0655f56b839e92
Joseph Luck <joseph.luck@sky.com>
2019-12-26 15:47:13 +0000

    Ensure readonly types for extra type safety

--- src/index.ts
+++ src/index.ts
@@ -6,39 +6,49 @@ export type RemoveFirstFromTuple<T extends any[]> = T["length"] extends 0
   ? I
   : [];
 
+type DeepReadonly<T> = T extends {}
+  ? {
+      readonly [P in keyof T]: T[P] extends {}
+        ? DeepReadonly<T[P]>
+        : Readonly<T>;
+    }
+  : Readonly<T>;
+
 const stately = <S>(state: S) => {
+  /** Represents an readonly version of S */
+  type IS = DeepReadonly<S>;
   type Unsubscribe = () => any;
-  type Subscription = (prevState: S, newState: S) => any;
+  type Subscription = (prevState: IS, newState: IS) => any;
 
   let _state = state;
   let _subscriptions: Subscription[] = [];
 
   const notifySubscribers = (prevState: S, newState: S) =>
-    _subscriptions.forEach(fn => fn(prevState, newState));
+    _subscriptions.forEach(fn => fn(prevState as IS, newState as IS));
 
   const createMutator = <Fn extends (state: S, ...args: any[]) => any>(
     fn: Fn
-  ) => (...args: RemoveFirstFromTuple<Parameters<typeof fn>>): S => {
+  ) => (...args: RemoveFirstFromTuple<Parameters<typeof fn>>): IS => {
     const newState = immer(_state, draft => {
       fn(draft as S, ...args);
     });
     notifySubscribers(_state, newState);
     _state = newState;
-    return newState;
+    return newState as IS;
   };
 
-  const createEffect = <Fn extends (state: S, ...args: any[]) => any>(
+  const createEffect = <Fn extends (state: IS, ...args: any[]) => any>(
     fn: Fn
   ) => (
     ...args: RemoveFirstFromTuple<Parameters<typeof fn>>
-  ): ReturnType<typeof fn> => fn(_state, ...args);
+  ): ReturnType<typeof fn> => fn(_state as IS, ...args);
 
-  const createSelector = <Fn extends (state: S, ...args: any[]) => any>(
+  const createSelector = <Fn extends (state: IS, ...args: any[]) => any>(
     fn: Fn
   ) => (
     ...args: RemoveFirstFromTuple<Parameters<typeof fn>>
   ): ReturnType<typeof fn> => {
-    return fn(_state, ...args);
+    return fn(_state as IS, ...args);
   };
 
   const subscribe = (sub: Subscription): Unsubscribe => {
