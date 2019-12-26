9c0553adbba7c28fdfb501719a2c837618774b46
Joseph Luck <joseph.luck@sky.com>
2019-12-26 17:05:07 +0000

    Expand react example to demonstrate selectors and component memoisation

--- examples/react/src/index.tsx
+++ examples/react/src/index.tsx
@@ -6,25 +6,56 @@ import makeUseStately from "@josephluck/stately/lib/hooks";
 
 // Set up the store:
 const store = stately({
-  count: 0
+  a: {
+    name: "Counter A",
+    count: 0
+  },
+  b: {
+    name: "Counter B",
+    count: 0
+  }
 });
 
+// Create some selectors to pick out memoised state from the store:
+const selectA = store.createSelector(s => s.a);
+const selectB = store.createSelector(s => s.b);
+
 // Create some mutators (these can be used directly in the component without hooks!):
-const increment = store.createMutator(s => s.count++);
-const decrement = store.createMutator(s => s.count--);
+const incrementA = store.createMutator(s => s.a.count++);
+const decrementA = store.createMutator(s => s.a.count--);
+const incrementB = store.createMutator(s => s.b.count++);
+const decrementB = store.createMutator(s => s.b.count--);
 
 // Create a custom hook using the stately hook factory:
 const useStately = makeUseStately(store);
 
+// Create a memoised component. Counter B should not re-render when Counter A's state changes
+interface CounterProps {
+  state: { name: string; count: number };
+  increment: () => void;
+  decrement: () => void;
+}
+const Counter = React.memo(({ state, increment, decrement }: CounterProps) => {
+  console.log(`Rendering counter ${name}`);
+  return (
+    <div>
+      <button onClick={decrement}>-</button>
+      <span>
+        {state.name}: {state.count}
+      </span>
+      <button onClick={increment}>+</button>
+    </div>
+  );
+});
+
 // Render the app and use the hook:
 const App = () => {
-  const count = useStately(s => s.count);
-
+  const a = useStately(selectA);
+  const b = useStately(selectB);
   return (
     <>
-      <button onClick={decrement}>-</button>
-      <span>{count}</span>
-      <button onClick={increment}>+</button>
+      <Counter state={a} increment={incrementA} decrement={decrementA} />
+      <Counter state={b} increment={incrementB} decrement={decrementB} />
     </>
   );
 };
